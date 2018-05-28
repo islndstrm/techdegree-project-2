6f2a418235e24ba70fe44eb614d4cf1a3bd5c2f4
Liz E <lizetchells@gmail.com>
2018-05-27 20:04:58 -0400

    added comments, cleaned up code

--- js/app.js
+++ js/app.js
@@ -5,12 +5,13 @@ const studentList = document.querySelectorAll('.student-list li');
 let pageNum = getNumberOfPages(studentList);
 const max = 10;
 
-// create elements
+// create elements for pagination and search bar
 const parent = document.querySelector(".page");
 const pageDiv = document.createElement('div');
 pageDiv.className = "pagination";
 parent.appendChild(pageDiv);
 let pageULs = document.createElement('ul');
+pageULs.className = "records";
 pageDiv.appendChild(pageULs);
 pageULs = document.querySelector('.pagination ul');
 const searchParent = document.querySelector('.page-header');
@@ -30,38 +31,32 @@ searchDiv.appendChild(searchButton);
 let messageDiv = document.createElement('div');
 messageDiv.className = "search-results";
 parent.insertBefore(messageDiv, pageDiv);
+let home = document.createElement('p');
+home.className = "home";
 
 // functions
 function getNumberOfPages (list) {
     return Math.floor(list.length / 10) + 1;
 }
 
+// creates the correct amount of page links
 function createPageLinks(count) {
   pageULs.innerHTML = "";
-  for (let i = 0; i < count; i++) {
+  for (let i = 0; i < count / 10; i++) {
     let li = document.createElement('li');
     pageULs.appendChild(li);
   }
   const pageLis = document.querySelectorAll('.pagination li');
-  for (let i = 0; i < count; i++) {
+  for (let i = 0; i < count / 10; i++) {
     pageLis[i].innerHTML = `<a href="#">${i+1}</a>`;
   }
+  // adds a return to home link
+  home.innerHTML = `<br>Return to <a href="#">home</a>`;
+  pageDiv.appendChild(home);
 }
-
-// function createSearchLinks(count) {
-//   for (let i = 0; i <= count; i++) {
-//     let li = document.createElement('li');
-//     messageDiv.appendChild(li);
-//   }
-//   const searchLis = document.querySelectorAll('.search-results li');
-//   for (let i = 0; i <= count; i++) {
-//     searchLis[i].innerHTML = `<a href="#">${i+1}</a>`;
-//   }
-// }
 
 //function shows which students should be show on the current page
 function showRecords(list, page) {
-
   // hide all records and clear messages
   for (let i = 0; i < list.length; i++) {
     list[i].style.display = "none";
@@ -76,10 +71,11 @@ function showRecords(list, page) {
     }
     list[i].style.display = "block";
   }
-  // create pagination
-  createPageLinks(pageNum);
+  // call pagination function
+  createPageLinks(list.length);
 }
 
+// search the student records and shows the results
 function searchList(terms) {
   // hide all records and clear messages
   for (let i = 0; i < studentList.length; i++) {
@@ -97,10 +93,13 @@ function searchList(terms) {
       message.push(studentList[i]);
     }
   }
+  // message for no results
   if (message.length === 0) {
     messageDiv.innerHTML = "<p>Sorry, no results found.</p>";
+    pageULs.innerHTML = "";
     return;
   }
+  // show results
   for (let i = 0; i < message.length; i++) {
     message[i].style.display = "block";
     parent.insertBefore(message[i], pageDiv);
@@ -118,9 +117,17 @@ pageULs.addEventListener('click', (event) => {
   showRecords(studentList, event.target.text);
 });
 
+// listens for the search box and runs the search function
 searchButton.addEventListener('click', () => {
-  const searchTerms = mySearch.value;
-  searchTerms.toLowerCase();
-  searchList(searchTerms);
-  mySearch.value = "";
+  if (mySearch.value != null) {
+    const searchTerms = mySearch.value;
+    searchTerms.toLowerCase();
+    searchList(searchTerms);
+    mySearch.value = "";
+  }
+});
+
+// returns to page 1 when the link is clicked
+home.addEventListener('click', () => {
+  showRecords(studentList, 1);
 });
